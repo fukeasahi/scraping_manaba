b811786f6e74d9af9b7ec3f3b59502a8b4f0f3fc
fukeasahi <fuke.asahi.infratop@gmail.com>
2021-08-07 09:55:44 +0900

    スクレイピングの機能完成

--- website/manaba.py
+++ website/manaba.py
@@ -38,88 +38,85 @@ def test():
 
 @manaba.route("/scraping")
 def scraping():
-    with open('private.pem', 'rb') as f:
-        private_pem = f.read()
-        private_key = RSA.import_key(private_pem)
-    decipher_rsa = PKCS1_OAEP.new(private_key)
+    try:
+        with open('private.pem', 'rb') as f:
+            private_pem = f.read()
+            private_key = RSA.import_key(private_pem)
+        decipher_rsa = PKCS1_OAEP.new(private_key)
 
-    users = User.query.filter_by(is_active=True)
-    for user in users:
-        # ここから暗号化解読
-        USER = decipher_rsa.decrypt(user.manaba_user_name).decode("utf-8")
-        PASS = decipher_rsa.decrypt(user.manaba_password).decode("utf-8")
-        api_token = decipher_rsa.decrypt(user.line_api_token).decode("utf-8")
-        # ここまで暗号化解読
+        users = User.query.filter_by(is_active=True)
+        for user in users:
+            # ここから暗号化解読
+            USER = decipher_rsa.decrypt(user.manaba_user_name).decode("utf-8")
+            PASS = decipher_rsa.decrypt(user.manaba_password).decode("utf-8")
+            api_token = decipher_rsa.decrypt(user.line_api_token).decode("utf-8")
+            # ここまで暗号化解読
 
-        browser = webdriver.Chrome(ChromeDriverManager().install())
-        browser.implicitly_wait(3)
+            browser = webdriver.Chrome(ChromeDriverManager().install())
+            browser.implicitly_wait(3)
 
-        url_login = "https://ct.ritsumei.ac.jp/ct/home"
-        browser.get(url_login)
-        time.sleep(3)
-        print("ログインページにアクセスしました。")
+            url_login = "https://ct.ritsumei.ac.jp/ct/home"
+            browser.get(url_login)
+            time.sleep(3)
 
-        elem = browser.find_element_by_id('User_ID')
-        elem.clear()
-        elem.send_keys(USER)
-        elem = browser.find_element_by_id('Password')
-        elem.clear()
-        elem.send_keys(PASS)
-        print("フォームを送信")
+            elem = browser.find_element_by_id('User_ID')
+            elem.clear()
+            elem.send_keys(USER)
+            elem = browser.find_element_by_id('Password')
+            elem.clear()
+            elem.send_keys(PASS)
 
-        # browser_from = browser.find_element_by_id('Submit')
-        # time.sleep(3)
-        # browser_from.click()
-        # print("情報を入力してログインボタンを押しました")
+            browser_from = browser.find_element_by_id('Submit')
+            time.sleep(3)
+            browser_from.click()
 
-        # url_home_whatsnew = "https://ct.ritsumei.ac.jp/ct/home_whatsnew"
-        # time.sleep(1)
-        # browser.get(url_home_whatsnew)
-        # print(url_home_whatsnew,":アクセス完了")
+            url_home_whatsnew = "https://ct.ritsumei.ac.jp/ct/home_whatsnew"
+            time.sleep(1)
+            browser.get(url_home_whatsnew)
 
-        # url_manaba = "https://ct.ritsumei.ac.jp/ct/"
+            url_manaba = "https://ct.ritsumei.ac.jp/ct/"
 
-        # def getYesterday():
-        #     today=datetime.date.today()
-        #     oneday=datetime.timedelta(days=1)
-        #     yesterday=today-oneday
-        #     return yesterday
+            def getYesterday():
+                today=datetime.date.today()
+                oneday=datetime.timedelta(days=1)
+                yesterday=today-oneday
+                return yesterday
 
-        # yesterday = getYesterday()
-        # date_yesterday = yesterday.strftime('%Y-%m-%d')
+            yesterday = getYesterday()
+            date_yesterday = yesterday.strftime('%Y-%m-%d')
 
-        # url_line = "https://notify-api.line.me/api/notify"
-        # api_token = ""# データベースからとってくる
-        # headers = {"Authorization": "Bearer " + api_token}
+            url_line = "https://notify-api.line.me/api/notify"
+            headers = {"Authorization": "Bearer " + api_token}
 
-        # def scraping(tag,class_name):
-        #     html = browser.page_source.encode('utf-8')
-        #     soup = BeautifulSoup(html, 'lxml')
-        #     results = soup.find_all(tag, class_=class_name)
-        #     return results
+            def scraping(tag,class_name):
+                html = browser.page_source.encode('utf-8')
+                soup = BeautifulSoup(html, 'lxml')
+                results = soup.find_all(tag, class_=class_name)
+                return results
 
-        # results = scraping("tr","row1")
-        # for result in results:
-        #     result_string = result.findAll("b")[0].string
-        #     result_string_str = str(result_string)
-        #     m = re.search("\d+-\d+-\d+", result_string_str)
-        #     msg_date = m.group()
-        #     print(msg_date,"ニュースの日付を取得しました。")
-        #     if date_yesterday == msg_date:
+            results = scraping("tr","row1")
+            for result in results:
+                result_string = result.findAll("b")[0].string
+                result_string_str = str(result_string)
+                m = re.search("\d+-\d+-\d+", result_string_str)
+                msg_date = m.group()
+                if date_yesterday == msg_date:
 
-        #         href = result.findAll("a")[0].get("href")
-        #         url_news_title = url_manaba + href
-        #         print(url_news_title)
+                    href = result.findAll("a")[0].get("href")
+                    url_news_title = url_manaba + href
 
-        #         msg_subject = result.findAll("a")[0].string
-        #         print(msg_subject)
+                    msg_subject = result.findAll("a")[0].string
 
-        #         message = [
-        #                 msg_subject,url_news_title
-        #         ]
-        #         payload = {'message': message}
-        #         r = requests.post(url_line, headers=headers, params=payload)
+                    message = [
+                            msg_subject,url_news_title
+                    ]
+                    payload = {'message': message}
+                    r = requests.post(url_line, headers=headers, params=payload)
 
-        browser.close()
-        browser.quit()
-    return "実行中"
+            browser.close()
+            browser.quit()
+    except:
+        pass
+    finally:
+        print('all finish')
+    return "スクレイピング実行中"
