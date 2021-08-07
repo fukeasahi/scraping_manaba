414394dcf25cbd1030e211886bc00cb030dbbd56
fukeasahi <fuke.asahi.infratop@gmail.com>
2021-08-07 14:36:44 +0900

    header footerを作成完了

--- website/views.py
+++ website/views.py
@@ -6,3 +6,7 @@ views = Blueprint("views", __name__)
 @views.route("/")
 def home():
   return render_template("home.html", user=current_user)
+
+@views.route("/line_api")
+def line_api():
+  return render_template("line_api.html", user=current_user)
