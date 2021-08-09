1bb2fa8b6c79c0ce70b6b19842e58e8c771df7e4
fukeasahi <fuke.asahi.infratop@gmail.com>
2021-08-09 11:32:12 +0900

    allow empty commit

--- website/auth.py
+++ website/auth.py
@@ -131,6 +131,6 @@ def sign_up():
             db.session.commit()
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
-            return redirect(url_for('views.home'))
+            return redirect(url_for('auth.show'))
 
     return render_template("sign_up.html", user=current_user)
