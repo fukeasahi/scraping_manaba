e0de5199fb13486a00fea2ebd2951b9daa1a9a49
fukeasahi <fuke.asahi.infratop@gmail.com>
2021-09-04 23:51:35 +0900

    fix

---
+++ website/worker.py
@@ -0,0 +1,17 @@
+
+import os
+
+import redis
+from rq import Worker, Connection
+
+print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa1")
+redis_url = os.environ['REDISTOGO_URL']
+print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa2")
+conn = redis.from_url(redis_url)
+print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa3")
+with Connection(conn):
+    print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa4")
+    worker = Worker(['default'])
+    print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa5")
+    worker.work()
+    print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa6")
