20ea514c2c7eacb234b8d071bc6a783ffb92c564
fukeasahi <fuke.asahi.infratop@gmail.com>
2021-09-05 13:41:14 +0900

    fix

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn app:app --log-file=-
-clock: python manaba.py
+clock: python website/manaba.py
--- website/manaba.py
+++ website/manaba.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .website.models import User
+from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash, pbkdf2_hex
 # from . import db
 from flask_login import login_user, login_required, logout_user, current_user
